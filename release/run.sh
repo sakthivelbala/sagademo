c2b1bd062994c77b99be4656ae2b8e5cbcb0e4c6
Sakthivel B <37621642+sakthivelbala@users.noreply.github.com>
2024-11-26 19:56:45 +0530

    Update run.sh

--- release/run.sh
+++ release/run.sh
@@ -2,13 +2,11 @@
 java -jar ./release/zipkin.jar &
 java -jar ./release/registry.jar &
 
-sleep 15
+sleep 10
 
+java -jar ./release/gateway.jar &
 java -jar ./release/cart.jar &
 java -jar ./release/order.jar &
 java -jar ./release/inventory.jar &
 java -jar ./release/payment.jar &
 
-sleep 15
-
-java -jar ./release/gateway.jar &
